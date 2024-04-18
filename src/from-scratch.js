d38789392647c99ddacfcc1d51335f26d7af974c
EngelsGB <engelsgarciabello@gmail.com>
2024-04-18 16:54:58 -0400

    Finished with short answers

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,34 +1,78 @@
-const helloWorldRegex = (str) => {};
+const helloWorldRegex = (str) => {
+  return /Hello world/i.test(str);
+};
 
-const hasAVowel = (str) => {};
+const hasAVowel = (str) => {
+  return /[aeiou]/i.test(str);
+};
 
-const hasCatsOrDogs = (str) => {};
+const hasCatsOrDogs = (str) => {
+  return /(cats|dogs)/i.test(str);
+};
 
-const hasVowelStart = (str) => {};
+const hasVowelStart = (str) => {
+  return /^[aeiou]/i.test(str);
+};
 
-const hasPunctuationEnd = (str) => {};
+const hasPunctuationEnd = (str) => {
+  return /[.!?]$/.test(str);
+};
 
-const hasNothingOrDigits = (str) => {};
+const hasNothingOrDigits = (str) => {
+  return !/[a-zA-Z]/gi.test(str);
+};
 
-const hasNoFlippers = (str) => {};
+const hasNoFlippers = (str) => {
+  return !/[BCcDEHIKOoXxl]/.test(str);
+};
 
-const isValidEmail = (str) => {};
+const isValidEmail = (str) => {
+  return /^[\w.]+@[\w]+\.[\w]{2,4}/.test(str);
+};
 
-const isValidPhoneNumber = (str) => {};
+const isValidPhoneNumber = (str) => {
+  return /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/.test(str);
+};
 
-const matchAllNumbers = (str) => {};
+const matchAllNumbers = (str) => {
+  if(str.match(/\d+/g)) return str.match(/\d+/g);
+  else return [];
+};
 
-const matchAllNumbersAsNumbers = (str) => {};
+const matchAllNumbersAsNumbers = (str) => {
+  const arr = str.match(/\d+/g);
+  if(arr === null) return [];
+  else {
+    for(const e in arr) {
+      arr[e] = Number(arr[e]);
+    }
+  }
+  return arr;
+};
 
-const matchAllWords = (str) => {};
+const matchAllWords = (str) => {
+  if(str.match(/[a-zA-Z\']+/g)) return str.match(/[a-zA-Z\']+/g);
+  else return [];
+};
 
-const replaceAllNumbers = (str) => {};
+const replaceAllNumbers = (str) => {
+  return str.replace(/\d+/g, "???");
+};
 
-const fixFileName = (str) => {};
+const fixFileName = (str) => {
+  return str.replaceAll(/\s+/g, "_");
+};
 
-const nameRedacter = (str) => {};
+const nameRedacter = (str) => {
+  return str.replaceAll(/[A-Z]{2,}/g, "REDACTED");
+};
 
-const camelToSnakeCase = (str) => {};
+const camelToSnakeCase = (str) => {
+  const convertToSnakeCase = (match, offset, string) => {
+    return (offset > 0 ? "_" : "") + match.toLowerCase();
+  }
+  return str.replaceAll(/[A-Z]/g, convertToSnakeCase);
+};
 
 module.exports = {
   helloWorldRegex,
